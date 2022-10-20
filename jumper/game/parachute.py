7c4c8863092e594a23fdf342b1939fd9cfda7aed
mvrush <mvrush@gmail.com>
2022-10-19 22:45:31 -0600

    Start parachute class

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,1 +1,12 @@
 # This is our parachute class
+
+class Parachute:
+    """The displayed parachute. 
+    
+    The parachute will slowly disappear as the result of missed guesses of the word. 
+    
+    Attributes:
+        _chute (list(str)) used to draw the parachute.
+        -man (list(str)) used to draw the man
+
+    """
