a55cb6ee5234a03740c168cd5cf1f24ba279bd2d
mvrush <mvrush@gmail.com>
2022-10-19 23:09:16 -0600

    add erase_chute() method

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -39,3 +39,21 @@ class Parachute:
         """
         for i in self._man: # This loops through the list and prints the item in each index position.
             print(i)
+    
+    def erase_chute(self, answer): # will receive 'answer' data from the Word class                
+        if answer == True:
+            for i in self._chute:
+                print(i)
+            for i in self._man:
+                print(i)
+        
+        if answer == False:
+            self._chute.pop(0) # Removes the item at index position 0. Note, if you don't specify an index position, pop() removes the last item on the list.
+            for i in self._chute:
+                print(i)
+            for i in self._man:
+                print(i)
+
+        if len(self._chute) == 0:
+            end_game = print(f"You fell!\n") # the \n adds a new blank line after this is printed. It's for formatting purposes to make it look better in the terminal.
+            return end_game # This 'return' statement takes effect only when this 'if' statement is reached and true. It ends the program.
