29ce2c1032c92e56767578a0dd613585b5960c1f
SakhileMamba <sakhymamba@gmail.com>
2022-10-22 01:51:25 +0200

    Encapsulation, made class attributes private

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -4,32 +4,32 @@ import random
 class Word:
 
     def __init__(self):
-        self.word_list = ["fox", "cat", "happy", "drama",
-                          "cartoons", "television", "biscuits", "wifi", "laptop"]
-        self.random_word = random.choice(self.word_list)
-        self.guess_state = []
+        self._word_list = ["fox", "cat", "happy", "drama",
+                           "cartoons", "television", "biscuits", "wifi", "laptop"]
+        self._random_word = random.choice(self._word_list)
+        self._guess_state = []
 
-        for x in self.random_word:
-            self.guess_state.append("_")
+        for x in self._random_word:
+            self._guess_state.append("_")
 
-        print(self.guess_state)  # Used in class test
+        print(self._guess_state)  # Used in class test
 
     def check_guess_matches(self, guessed_character):
-        if guessed_character in self.random_word:
+        if guessed_character in self._random_word:
             i = 0
-            for x in self.random_word:
+            for x in self._random_word:
                 if x == guessed_character:
-                    self.guess_state[i] = x
+                    self._guess_state[i] = x
                 i += 1
 
-            print(self.guess_state)  # Used in class test
-            print("".join(self.guess_state))  # Used in class test
+            print(self._guess_state)  # Used in class test
+            print("".join(self._guess_state))  # Used in class test
             return True
         else:
             return False
 
     def word_match_complete(self):
-        if "".join(self.guess_state) == self.random_word:
+        if "".join(self._guess_state) == self._random_word:
             return True
         else:
             return False
